84aa8e0c1c7583ae01475c4578b5df492b588ab0
nbrown0000 <brown.nathanjames@gmail.com>
2019-07-16 18:54:00 +1000

    first upload of files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Periodic Table of Elements</h1>
+      {/* <PeriodicTable/> */}
+    </div>
+  );
+}
+
+export default App;
