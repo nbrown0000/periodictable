bdc095c99fa1898c05be2621dd5859babad9bc2f
nbrown0000 <brown.nathanjames@gmail.com>
2019-10-18 15:58:04 +1100

    Updates

---
+++ precache-manifest.0b8fa3f2a20bf30779a3ee7f871085c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa46a65c48348f4e990e9168b8b135b",
+    "url": "/periodictable/index.html"
+  },
+  {
+    "revision": "d9c941d3981cdfc9d715",
+    "url": "/periodictable/static/css/main.d7976cf4.chunk.css"
+  },
+  {
+    "revision": "12a18aa7387cf1894cdb",
+    "url": "/periodictable/static/js/2.0144a7d6.chunk.js"
+  },
+  {
+    "revision": "d9c941d3981cdfc9d715",
+    "url": "/periodictable/static/js/main.71a0e2f1.chunk.js"
+  },
+  {
+    "revision": "a619d6c28d7d90cb9ccd",
+    "url": "/periodictable/static/js/runtime~main.97cf86b6.js"
+  }
+]);
